654bb1855ab73d10e7c9380cf6a9e55546a7509b
Aman Mathur <mathuraman14@hotmail.com>
2022-07-19 19:12:33 +0530

    Changes Commit

--- src/Multithreading/Singleton.java
+++ src/Multithreading/Singleton.java
@@ -1,4 +1,33 @@
 package Multithreading;
 
 public class Singleton {
+    private static Singleton object;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+
+        if(object == null) {
+            object = new Singleton();
+        }
+
+
+        return object;
+    }
+
+    public void print() {
+        System.out.println("Your Program is Up and Running.");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Singleton db1;
+
+        // refers to the only object of Singleton
+        db1= Singleton.getInstance();
+
+        db1.print();
+    }
 }
