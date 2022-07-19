1e8808bfced1687de97a354ea1cb0e4a090003e7
Aman Mathur <mathuraman14@hotmail.com>
2022-07-19 18:58:40 +0530

    Changes Commit

--- src/BasicJava/NumberOfOccurencies.java
+++ src/BasicJava/NumberOfOccurencies.java
@@ -1,4 +1,27 @@
 package BasicJava;
 
+import java.util.Scanner;
+
 public class NumberOfOccurencies {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n;
+        n = scan.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = scan.nextInt();
+        }
+
+        for (int i=0;i<n;i++)
+        {   int count =0;
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j]) {
+                    count++;
+                }
+            }
+            System.out.println(arr[i] + " - - > > " + count);
+        }
+    }
 }
