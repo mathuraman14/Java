7d7ccbb080689abc6ffbd048a6188143212647ef
Aman Mathur <mathuraman14@hotmail.com>
2022-07-23 00:00:51 +0530

    Changes Commit

--- src/Strings/GetCharacterfromString.java
+++ src/Strings/GetCharacterfromString.java
@@ -1,4 +1,15 @@
 package Strings;
 
+import java.util.Scanner;
+
 public class GetCharacterfromString {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter String without spaces: ");
+        String str = scan.next();
+        System.out.print("Enter index at which you want to fetch character: ");
+        int n = scan.nextInt();
+        char c = str.toCharArray()[n];
+        System.out.println("Character at index " + n + " is " + c);
+    }
 }
